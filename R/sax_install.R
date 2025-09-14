12175fa8ae5d67665ddc9256a88cd84f873eca96
Jean Palate <palatej@users.noreply.github.com>
2025-09-14 21:46:25 +0200

    Install

---
+++ R/sax_install.R
@@ -0,0 +1,4 @@
+install.packages("remotes")
+#remotes::install_github("rjdverse/rjd3toolkit@*release")
+remotes::install_github("palatej/rjd3sax")
+remotes::install_github("rjdverse/rjd3sts@*release")
